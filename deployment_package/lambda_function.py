61fffce413c51f8223df4ee3a8c8165124846ba7
otgon <otgon.petru@gmail.com>
2023-08-27 19:07:50 +0200

    Deployment package for S3

---
+++ deployment_package/lambda_function.py
@@ -0,0 +1,131 @@
+import json
+import logging
+import utils
+import ask_sdk_core.utils as ask_utils
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model import Response
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)   
+    
+def build_response(output):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'version': '1.0',
+            'response': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': output
+                },
+                'shouldEndSession': True
+            }
+        })
+    }
+    
+    
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input) -> Response:
+        # type: (HandlerInput) -> Response
+        greet_text = utils.greetResponse()
+        reprompt_text = "How can I help you?"
+        return handler_input.response_builder.speak(greet_text).reprompt(reprompt_text).response
+    
+class GPTIntentHandler(AbstractRequestHandler):
+    """Handler for GPT intent."""
+    def can_handler(self, handler_input):
+        return ask_utils.is_intent_name("GPTIntent")(handler_input) or ask_utils.is_intent_name("AMAZON.FallbackIntent")(handler_input)
+    
+    def handler(self, handler_input):
+        if ask_utils.is_intent_name("GPTIntent")(handler_input):
+            query = handler_input.request_envelope.request.intent.slots["Query"].value
+        else:
+            query = handler_input.request_envelope.request.intent.query
+        
+        speech_text = utils.make_prompt(query)
+        return handler_input.response_builder.speak(speech_text).response
+    
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        pass
+        
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .response
+        )
+        
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        intent_name = ask_utils.get_intent_name(handler_input)
+        response_text = f"You just triggered {intent_name}."
+        return build_response(response_text)
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        return True
+
+    def handle(self, handler_input, exception):
+        logger.error(exception, exc_info=True)
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .response
+        )
+
+sb = SkillBuilder()
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+lambda_handler = sb.lambda_handler()
---
+++ deployment_package/utils.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import openai
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def greetResponse():
+    current_time = datetime.datetime.now().time().hour
+    if current_time >= 6 or current_time <= 12: 
+        return "Good morning! How may I assist you?"
+    elif current_time > 12 or current_time <= 19:
+        return "Good afternoon! How may I assist you?"
+    elif current_time > 19 or current_time < 6:
+        return "Good evening! How may I assist you?"
+    
+    
+def get_secret():
+    """Extracts the OpenAI API key from the AWS' Secrets Manager tool
+
+    Raises:
+        e: Secret extraction generic error
+
+    Returns:
+        str: OpenAI API key
+    """
+
+    secret_name = "openai_api_key"
+    region_name = "eu-west-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        logging.error("Exception extracting secret: %s", str(e))
+        
+    return get_secret_value_response['SecretString']
+
+def make_prompt(prompt, model = "gpt-3.5-turbo", temperature = 0.7):
+    """Sends the prompt to GPT using gpt-3.5-turbo as the default model with a default temperature of 0.7
+
+    Args:
+        prompt (str): Prompt that will be sent to OpenAI
+        model (str, optional): OpenAI language model. Defaults to "gpt-3.5-turbo".
+        temperature (int, optional): Prompt temperature. Determines the randomness of the response. Defaults to 0.7
+
+    Returns:
+        str: OpenAI's response
+    """
+    
+    openai.api_key = get_secret()
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=temperature
+    )
+    return response['choices'][0]['message']['content']
